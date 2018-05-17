562a7a7444712d8f9e0e57396f2cf6903f687c5e
Norrisanator <matthew.norris96@gmail.com>
2018-05-17 20:07:43 +1200

    Initial Commit

---
+++ Core/DungeonMap.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RogueSharp;
+using RLNET;
+
+namespace RogueGame.Core
+{
+    // dungeon extends map
+    public class DungeonMap : Map
+    {
+        //renders all symbols/colors for each cell
+        public void draw( RLConsole mapConsole)
+        {
+            foreach(Cell cell in GetAllCells())
+            {
+                SetConsoleSymbolForCell(mapConsole, cell);
+            }
+        }
+
+        private void SetConsoleSymbolForCell(RLConsole mapConsole, Cell cell)
+        {
+            if (!cell.IsExplored)
+            {
+                return;
+            }
+            if( IsInFov( cell.X, cell.Y))
+            {
+                if (cell.IsWalkable)
+                {
+                    mapConsole.Set(cell.X, cell.Y, Colors.FloorFov, Colors.FloorBackgroundFov, '.');
+                }
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using RLNET;
+using RogueGame.Core;
+
+namespace RogueGame
+{
+    public class Program
+    {
+        // Screen console width/height measured in number of tiles
+        private static readonly int _screenWidth = 100;
+        private static readonly int _screenHeight = 70;
+        private static RLRootConsole _rootConsole;
+
+        // Map console width/height measured in number of tiles
+        private static readonly int _mapWidth = 80;
+        private static readonly int _mapHeight = 48;
+        private static RLConsole _mapConsole;
+
+        // Message console below map width/height measured in number of tiles.
+        private static readonly int _messageWidth = 80;
+        private static readonly int _messageHeight = 11;
+        private static RLConsole _messageConsole;
+
+        // Stat console right of map width/height measured in number of tiles. 
+        private static readonly int _statWidth = 20;
+        private static readonly int _statHeight = 70;
+        private static RLConsole _statConsole;
+
+        // Inventory console above map width/height measured in number of tiles. 
+        private static readonly int _inventoryWidth = 80;
+        private static readonly int _inventoryHeight = 11;
+        private static RLConsole _inventoryConsole;
+
+
+        static void Main(string[] args)
+        {
+            string fontFileName = "terminal8x8.png";
+            string windowName = "Rogue Game";
+
+            _rootConsole = new RLRootConsole(fontFileName, _screenWidth, _screenHeight, 8, 8, 1f, windowName);
+
+            _mapConsole = new RLConsole(_mapWidth, _screenHeight);
+            _messageConsole = new RLConsole(_messageWidth, _messageHeight);
+            _statConsole = new RLConsole(_statWidth, _statHeight);
+            _inventoryConsole = new RLConsole(_inventoryWidth, _inventoryHeight);
+
+            _rootConsole.Update += OnRootConsoleUpdate;
+            _rootConsole.Render += OnRootConsoleRender;
+            _rootConsole.Run();
+        }
+
+        private static void OnRootConsoleUpdate(object sender, UpdateEventArgs e)
+        {
+            _mapConsole.SetBackColor(0, 0, _mapWidth, _mapHeight, Colors.FloorBackground);
+            _mapConsole.Print(1, 1, "Map", Colors.TextHeading);
+
+            _messageConsole.SetBackColor(0, 0, _messageWidth, _messageHeight, Swatch.DbDeepWater);
+            _messageConsole.Print(1, 1, "Message", Colors.TextHeading);
+
+            _statConsole.SetBackColor(0, 0, _statWidth, _statHeight, Swatch.DbOldStone);
+            _statConsole.Print(1, 1, "Stat", Colors.TextHeading);
+
+            _inventoryConsole.SetBackColor(0, 0, _inventoryWidth, _inventoryHeight, Swatch.DbWood);
+            _inventoryConsole.Print(1, 1, "Invetory", Colors.TextHeading);
+        }
+
+        private static void OnRootConsoleRender(object sender, UpdateEventArgs e)
+        {
+            // Blits other consoles to root console
+            RLConsole.Blit(_mapConsole, 0, 0, _mapWidth, _mapHeight, _rootConsole, 0, _inventoryHeight);
+            RLConsole.Blit(_statConsole, 0, 0, _statWidth, _statHeight, _rootConsole, _mapWidth, 0);
+            RLConsole.Blit(_messageConsole, 0, 0, _messageWidth, _messageHeight, _rootConsole, 0, _screenHeight - _messageHeight);
+            RLConsole.Blit(_inventoryConsole, 0, 0, _inventoryWidth, _inventoryHeight, _rootConsole, 0, 0);
+            // draws console to screen
+            _rootConsole.Draw();
+        }
+    }
+}
